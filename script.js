c77244a80a04190f8fbb0fe44494126dbb1437b3
Mei <LiMei@tutamail.com>
2025-08-17 14:55:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/esm/supabase.min.js';
 // --- Supabase Setup ---
 const SUPABASE_URL = "https://lrmfhusbakkgpjjdjdvg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxybWZodXNiYWtrZ3BqamRqZHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyOTQzNTUsImV4cCI6MjA3MDg3MDM1NX0.bY9ILZaTNELGjRvu7ovcKA2moqnOhAb_8oN2QhIigPg";
